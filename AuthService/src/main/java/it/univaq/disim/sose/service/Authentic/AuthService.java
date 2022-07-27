1353985105d17f76ed297c62a5873f66425a5fa1
alessandrodimatteo97 <achissimo@gmail.com>
2022-07-27 19:16:49 +0200

    added swagger to AuthService

--- AuthService/src/main/java/it/univaq/disim/sose/service/Authentic/AuthService.java
+++ AuthService/src/main/java/it/univaq/disim/sose/service/Authentic/AuthService.java
@@ -9,6 +9,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
 
 
 @Path("User")
@@ -16,26 +20,57 @@ public interface AuthService {
 
 
 
-	
+	@Operation( 
+		       description = "Login operation by specifying username and password",
+				responses = {
+			       @ApiResponse(
+			          description = "Return the Token for the user logged",
+			          content = {
+			                 @Content(mediaType = MediaType.APPLICATION_JSON)
+			          }
+			       )
+			    }
+			 )
 	@POST
 	@Produces({MediaType.APPLICATION_JSON})
 	@Path("/Login")
 	String login(@FormParam("username") String username, @FormParam("password") String password ) throws SQLException;
 	
+	
+	@Operation( 
+		       description = "Signup operation by specifying username and password",
+				responses = {
+			       @ApiResponse(
+			          description = "Return the id for the registered user",
+			          content = {
+			                 @Content(mediaType = MediaType.APPLICATION_JSON)
+			          }
+			       )
+			    }
+			 )
 	@POST
 	@Produces({MediaType.APPLICATION_JSON})
 	@Path("/signup")
 	String signup(@FormParam("username") String username, @FormParam("password") String password ) throws SQLException;
 	
+	
+	@Operation( 
+		       description = "Check the token of the user by specifying userID and userToken",
+				responses = {
+			       @ApiResponse(
+			          description = "Return true if logged, false otherwise",
+			          content = {
+			                 @Content(mediaType = MediaType.APPLICATION_JSON)
+			          }
+			       )
+			    }
+			 )
 	@POST
 	@Produces({MediaType.APPLICATION_JSON})
 	@Path("/checkUser")
 	String checkUser(@FormParam("userID") int userID, @FormParam("userToken") String userToken) throws SQLException;
-	
-	
-	
-	
 
+		
 	
 	
 	
