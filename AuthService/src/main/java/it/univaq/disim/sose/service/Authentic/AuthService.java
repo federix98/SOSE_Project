83f6fb49a15670ade50e9f8ed683cf522d8e7ff8
alessandrodimatteo97 <achissimo@gmail.com>
2022-07-27 18:49:21 +0200

    Merge branch 'main' of https://github.com/federix98/SOSE_Project into main

--- AuthService/src/main/java/it/univaq/disim/sose/service/Authentic/AuthService.java
+++ AuthService/src/main/java/it/univaq/disim/sose/service/Authentic/AuthService.java
@@ -3,11 +3,10 @@ package it.univaq.disim.sose.service.Authentic;
 
 import java.sql.SQLException;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 
@@ -21,17 +20,17 @@ public interface AuthService {
 	@POST
 	@Produces({MediaType.APPLICATION_JSON})
 	@Path("/Login")
-	String login(@PathParam("username") String username, @PathParam("password") String password ) throws SQLException;
+	String login(@FormParam("username") String username, @FormParam("password") String password ) throws SQLException;
 	
 	@POST
 	@Produces({MediaType.APPLICATION_JSON})
 	@Path("/signup")
-	String signup(@QueryParam("username") String username, @QueryParam("password") String password ) throws SQLException;
+	String signup(@FormParam("username") String username, @FormParam("password") String password ) throws SQLException;
 	
 	@POST
 	@Produces({MediaType.APPLICATION_JSON})
 	@Path("/checkUser")
-	String checkUser(@QueryParam("userID") int userID, @QueryParam("userToken") String userToken) throws SQLException;
+	String checkUser(@FormParam("userID") int userID, @FormParam("userToken") String userToken) throws SQLException;
 	
 	
 	
